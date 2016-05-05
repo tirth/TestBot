d934884c481d5526692882ee4302ca031d6dc191
Tirth <complaints@tirthpatel.com>
2016-05-05 00:41:20 -0400

    Initial commit

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Web.Http;
-using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
-using Microsoft.Bot.Connector.Utilities;
-using Newtonsoft.Json;
 
 namespace TestBot
 {
@@ -26,7 +19,7 @@ namespace TestBot
                 int length = (message.Text ?? string.Empty).Length;
 
                 // return our reply to the user
-                return message.CreateReplyMessage($"You sent {length} characters");
+                return message.CreateReplyMessage($"You sent {length} characters, chump.");
             }
             else
             {
